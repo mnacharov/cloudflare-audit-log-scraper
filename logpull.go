515001c6e608260dc49055ac28575bfb2e1e2767
Mikhail Nacharov <1246847+mnacharov@users.noreply.github.com>
2025-12-10 17:28:13 +0100

    feat: message dns changes to slack channel

--- logpull.go
+++ logpull.go
@@ -1,85 +1,166 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
-	"github.com/cloudflare/cloudflare-go"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
-
-const fileName = "lastProcessed.txt"
-const maxLookBack = 360 // 6 hours in minutes
 
 var (
 	logsProcessed = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "cloudflare_audit_logs_processed_total",
 		Help: "The total number of processed events",
 	})
+	logsProcessedErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "cloudflare_audit_logs_processed_errors_total",
+		Help: "The total number of errors during processing events",
+	})
+	dnsRecords = make(map[string]AccountAuditLogsResourceDNSResponse)
+	lastProcessed = time.Now().Add(time.Duration(-5) * time.Minute).UTC().Format(time.RFC3339)
 )
 
-// Get audit logs and process them until no more records are returned
-func getAuditLogs(apiKey, apiEmail, orgId, s3Bucket string, lookBack int) error {
-	api, err := cloudflare.New(apiKey, apiEmail)
-	if err != nil {
-		return fmt.Errorf("error creating Cloudflare API client: %v", err)
-	}
-
-	ctx := context.Background()
-	userDetails, err := api.UserDetails(ctx)
-	if err != nil {
-		return err
-	}
-
-	if len(userDetails.Email) == 0 {
-		return nil
-	}
-
-	var searchUntil time.Time
-	if len(s3Bucket) > 0 {
-		searchUntil, err = getLastProcessedTimeFromS3(lookBack, s3Bucket, fileName)
-	} else {
-		searchUntil, err = getLastProcessedTime(lookBack, fileName)
-	}
-	if err != nil {
-		return err
-	}
-
-	if err := processAuditLogs(ctx, api, orgId, searchUntil); err != nil {
-		return err
-	}
-
-	if len(s3Bucket) > 0 {
-		return storeLastProcessedTimeToS3(time.Now(), s3Bucket, fileName)
-	}
-	return storeLastProcessedTimeToDisk(time.Now(), fileName)
-}
-
-func processAuditLogs(ctx context.Context, api *cloudflare.API, orgId string, searchUntil time.Time) error {
-	pageNumber := 1
-	for {
-		filterOpts := cloudflare.AuditLogFilter{Since: searchUntil.Format(time.RFC3339), Page: pageNumber}
-		results, err := api.GetOrganizationAuditLogs(ctx, orgId, filterOpts)
+func initDNSRecord(apiKey, orgId string, zones []string) error {
+	var failed bool
+	client := &http.Client{}
+	for _, zone_id := range zones {
+		req, err := http.NewRequest("GET", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records?per_page=5000000", zone_id), nil)
 		if err != nil {
-			return fmt.Errorf("error getting audit logs: %v", err)
+			log.Fatalf("Error creating request to Cloudflare API: %v", err)
 		}
-
-		if len(results.Result) == 0 {
-			break
+		req.Header = http.Header{
+			"Authorization": {fmt.Sprintf("Bearer %s", apiKey)},
 		}
-
-		for _, record := range results.Result {
-			b, err := json.Marshal(record)
-			if err != nil {
-				return err
-			}
-			logsProcessed.Inc()
-			fmt.Println(string(b))
+		res, err := client.Do(req)
+		if err != nil {
+			log.Printf("Error executing request to Cloudflare API: %v\n", err)
+			failed = true
+			continue
 		}
-		pageNumber++
+		var listDNSRecords ListDNSRecords
+		err = json.NewDecoder(res.Body).Decode(&listDNSRecords)
+		if err != nil {
+			log.Printf("Error decoding response from Cloudflare API: %v\n", err)
+			failed = true
+			continue
+		}
+		if !listDNSRecords.Success {
+			log.Printf("Error accessing Cloudflare API: %+v\n", listDNSRecords.Errors)
+			failed = true
+			continue
+		}
+		for _, record := range listDNSRecords.Result {
+			dnsRecords[record.ID] = record
+		}
+	}
+	if failed {
+		return errors.New("Some of the zones aren't populated")
 	}
 	return nil
+}
+// Get audit logs and process them until no more records are returned
+func getAuditLogs(apiKey, orgId, slackWebhook string, zones []string) error {
+	client := &http.Client{}
+	before := time.Now().UTC().Format(time.RFC3339)
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/accounts/%s/logs/audit?limit=1000&direction=asc&action_type.not=view&resource_product=dns_records&since=%s&before=%s",
+		orgId, lastProcessed, before)
+	for _, z := range zones {
+		url = fmt.Sprintf("%s&zone_id=%s", url, z)
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalf("Error creating request to Cloudflare API: %v", err)
+	}
+	req.Header = http.Header{
+		"Authorization": {fmt.Sprintf("Bearer %s", apiKey)},
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	var auditLogs AccountAuditLogs
+	err = json.NewDecoder(res.Body).Decode(&auditLogs)
+	if err != nil {
+		return err
+	}
+	if !auditLogs.Success {
+		return fmt.Errorf("ERROR: %+v\n", auditLogs)
+	}
+	for _, err := range auditLogs.Errors {
+		log.Printf("ERROR: %s\n", err.Error())
+	}
+	for _, auditLog := range auditLogs.Result {
+		err = processAuditLog(auditLog, slackWebhook)
+		if err != nil {
+			log.Printf("ERROR: %s\n", err)
+			logsProcessedErrors.Inc()
+			continue
+		} else {
+			logsProcessed.Inc()
+		}
+		lastProcessed = auditLog.Action.Time
+	}
+	// one milliseconds *after* the last shown record
+	t, err := time.Parse(time.RFC3339, lastProcessed)
+	if err != nil {
+		return err
+	}
+	lastProcessed = t.Add(1*time.Second).Format(time.RFC3339)
+	return nil
+}
+
+func processAuditLog(auditLog AccountAuditLogsResult, slackWebhook string) error {
+	// fmt.Printf("Result ID: %s\n", log.ID)
+	if auditLog.Resource.Product != "dns_records" {
+		return fmt.Errorf("Not supported audit record product %s", auditLog.Resource.Product)
+	}
+	var response AccountAuditLogsResourceDNSResponse
+	json.Unmarshal(auditLog.Resource.Response, &response)
+	if response.ID == "" {
+		// cloudflare can return empty response for recently created records
+		return fmt.Errorf("Invalid response: %v", auditLog.Resource.Response)
+	}
+	if auditLog.Action.Result == "success" {
+		if auditLog.Action.Type == "create" {
+			dnsRecords[response.ID] = response
+		}
+		if auditLog.Action.Type == "delete" {
+			value, ok := dnsRecords[response.ID]
+			if ok {
+				response = value  // to output what was in the deleted record
+			}
+		}
+	}
+	if auditLog.Action.Type == "create" || auditLog.Action.Type == "delete" || auditLog.Action.Type == "update" {
+		return slackMessage(response, auditLog, slackWebhook)
+	}
+	return nil
+}
+
+func slackMessage(response AccountAuditLogsResourceDNSResponse, auditLog AccountAuditLogsResult, slackWebhook string) error {
+	// Build readable Slack message
+	text := fmt.Sprintf(
+                "*Cloudflare %s (%s)*\n"+
+			"*Time:*        %s\n"+
+			"*Actor:*       %s (%s)\n"+
+			"*Zone:*        %s\n"+
+			"*DNS:*         %s\t%s\t%s\t(%s)\n"+
+			"*IsProxied:*  %t\n",
+                auditLog.Action.Description, auditLog.Action.Result,
+                auditLog.Action.Time,
+                auditLog.Actor.Email, auditLog.Actor.Type,
+		auditLog.Zone.Name,
+                response.Type, response.Name, response.Content, response.ID,
+                response.Proxied,
+	)
+	msg := map[string]string{"text": text}
+	payload, _ := json.Marshal(msg)
+	_, err := http.Post(slackWebhook, "application/json", bytes.NewBuffer(payload))
+	return err
 }
--- main.go
+++ main.go
@@ -1,58 +1,76 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+	"strconv"
+	"strings"
 
 	"github.com/go-co-op/gocron"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func runCronJobs(apiKey, apiEmail, orgId, s3Bucket string, lookBack int) {
+func runCronJobs(apiKey, orgId, slackWebhook string, zones []string, lookupInteval int) {
 	s := gocron.NewScheduler(time.UTC)
 
-	s.Every(lookBack).Minute().Do(func() {
-		getAuditLogs(apiKey, apiEmail, orgId, s3Bucket, lookBack)
+	s.Every(lookupInteval).Second().Do(func() {
+		err := getAuditLogs(apiKey, orgId, slackWebhook, zones)
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 
 	s.StartBlocking()
-	fmt.Println()
 }
 
 func main() {
-	apiEmail := os.Getenv("CLOUDFLARE_API_EMAIL")
 	apiKey := os.Getenv("CLOUDFLARE_API_KEY")
 	orgId := os.Getenv("CLOUDFLARE_ORGANIZATION_ID")
-	interval := os.Getenv("CLOUDFLARE_LOOK_BACK_INTERVAL")
-	s3Bucket := os.Getenv("AWS_S3_BUCKET_NAME")
+	interval := os.Getenv("CLOUDFLARE_LOOKUP_INTERVAL")
+	slackWebhook := os.Getenv("SLACK_WEBHOOK")
+	zones := os.Getenv("CLOUDFLARE_ZONE_IDS")
 
-	if apiEmail == "" {
-		log.Fatal("Must specify CLOUDFLARE_API_EMAIL")
-	}
 	if apiKey == "" {
 		log.Fatal("Must specify CLOUDFLARE_API_KEY")
 	}
 	if orgId == "" {
 		log.Fatal("Must specify CLOUDFLARE_ORGANIZATION_ID")
 	}
-
-	// Convert user supplied look back value to an integer otherwise set a default
-	var lookBack int
-	if interval != "" {
-		envToInt, err := getEnvInt(interval)
-		if err != nil {
-			log.Fatal("If CLOUDFLARE_LOOK_BACK_INTERVAL it must be an integer")
-		}
-		lookBack = envToInt
+	if slackWebhook == "" {
+		log.Fatal("Must specify SLACK_WEBHOOK")
 	}
-	lookBack = 5
+	if orgId == "" {
+		log.Fatal("Must specify CLOUDFLARE_ORGANIZATION_ID")
+	}
+
+
+	
+	// Convert user supplied look back value to an integer otherwise set a default
+	var lookupInterval int
+	if interval != "" {
+		var err error
+		lookupInterval, err = strconv.Atoi(interval)
+		if err != nil {
+			log.Fatal("CLOUDFLARE_LOOKUP_INTERVAL must be an integer", err)
+		}
+	} else {
+		lookupInterval = 300
+	}
 
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
 		http.ListenAndServe(":2112", nil)
 	}()
-	runCronJobs(apiKey, apiEmail, orgId, s3Bucket, lookBack)
+	if len(zones) > 0 {
+		err := initDNSRecord(apiKey, orgId, strings.Split(zones, ","))
+		if err != nil {
+			log.Printf("ERROR: failed to list DNS records (deleted records will have only IDs) %s\n", err)
+		}
+	} else {
+		log.Println("WARN: CLOUDFLARE_ZONES not specified, deleted records will have only IDs")
+	}
+
+	runCronJobs(apiKey, orgId, slackWebhook, strings.Split(zones, ","), lookupInterval)
 }
